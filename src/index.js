4e60fa3f4947dfa84e18edc576e92630b5148069
uladzimir <vova108vova@gmail.com>
2023-06-17 17:49:31 +0300

    DOM elements with useRef hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Ref from './UseRef'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const root = createRoot(document.getElementById('root'))
-root.render(<App/>)
+root.render(
+  <>
+    <App />
+    <Ref />
+  </>
+)
