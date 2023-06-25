1d927490ef1d079c6ed2a0fa9a0cb22306b401e5
uladzimir <vova108vova@gmail.com>
2023-06-25 17:04:50 +0300

    added useInput custom hook

--- src/FormHook.js
+++ src/FormHook.js
@@ -1,38 +1,45 @@
-import { useState, useEffect} from 'react';
-import {Container} from 'react-bootstrap';
-import './App.css';
+import {useEffect} from 'react'
+import { Container } from 'react-bootstrap'
+import useInput from './hooks/useInput'
+import './App.css'
 
 const FormHook = () => {
-  const [text, setText] = useState('');
-  function validateText(txt) {
-    return txt.search(/\d/) < 0
-  }
-  const styles = {
-    color: {
-      green: 'green',
-      red: 'red',
-    }
-  }
-  const color = validateText(text) ? styles.color.green : styles.color.red
+  const text = useInput('')
+  const textAr = useInput('')
+  const rezult = useInput('')
+  useEffect(() => {
+    const input = document.querySelector('#inputRez')
+    const color = input.value.search(/\d/) < 0 ? "green" : "red"
+    input.style.color = color
+  })
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
-                <div className="mb-3">
-                    <input value={text} type="text" className="form-control" readOnly/>
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input
-                      onChange={(e) => setText(e.target.value)}
-                      type="email"
-                      className="form-control"
-                      style={{color}}
-                      id="exampleFormControlInput1"
-                      placeholder="name@example.com"
-                      value={text}
-                    />
-                    </div>
-                    <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <div className="mb-3">
+            <input
+              id='inputRez'
+              value={`${text.value} / ${textAr.value}`}
+              onChange={rezult.changeValue}
+              type="text" className="form-control" readOnly />
+                  <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                  <input
+                    onChange={text.changeValue}
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                    value={text.value}
+                  />
+                  </div>
+                  <div className="mb-3">
+                  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea
+                value={textAr.value}
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                onChange={textAr.changeValue}
+              ></textarea>
                 </div>
             </form>
         </Container>
